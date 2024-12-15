00e8256f5c0035b08661a8265d45c4540003e24b
Yashodhan Pise <me-coder@users.noreply.github.com>
2024-12-16 02:27:04 +0530

    Eliminate need for explicit TLS key generation (#2)
    Eliminate need for explicit TLS key generation (#2)

--- ec2/main.tf
+++ ec2/main.tf
@@ -23,24 +23,33 @@ resource "aws_default_subnet" "default" {
 }
 
 # Dynamically generate a new private key on AWS [Doesn't work as expected]
-# resource "tls_private_key" "ssh_key" {
-#   algorithm = "ED25519"
-#   # algorithm = "RSA"
-#   # rsa_bits = 4096
-# }
+resource "tls_private_key" "ssh_key" {
+  # algorithm = "ED25519"
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
 resource "aws_key_pair" "ssh_key_pair" {
   key_name = var.key_name
   # Use the local public key file
-  public_key = file(".ssh/${var.key_name}.pem.pub")
+  # public_key = file(".ssh/${var.key_name}.pem.pub")
+  public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
 # Save the dynamically generated key pair pem file
-# resource "local_sensitive_file" "pem_file" {
-#   filename        = "${path.module}/${var.key_name}.pem"
-#   file_permission = "400"
-#   # directory_permission = "700"
-#   content = tls_private_key.ssh_key.private_key_pem
+resource "local_sensitive_file" "pem_file" {
+  filename             = "${path.module}/${var.key_name}.pem"
+  file_permission      = "400"
+  directory_permission = "755"
+  content              = tls_private_key.ssh_key.private_key_pem
+}
+
+# Maintained for debugging purpose
+# resource "local_sensitive_file" "pem_pub_file" {
+#   filename             = "${path.module}/${var.key_name}.pem.pub"
+#   file_permission      = "644"
+#   directory_permission = "755"
+#   content              = tls_private_key.ssh_key.public_key_openssh
 # }
 
 resource "aws_security_group" "allow_ssh" {
@@ -106,14 +115,13 @@ resource "aws_instance" "linux_server" {
         echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config.d/${var.tag_name}.conf
         echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config.d/${var.tag_name}.conf
         systemctl restart sshd
-        echo "HISTCONTROL=ignoreboth" >> ~/.bash_profile
+        echo "HISTCONTROL=ignoreboth" >> /home/ec2-user/.bash_profile
   EOF
 
   connection {
-    type = "ssh"
-    user = "ec2-user"
-    # private_key = tls_private_key.ssh_key.private_key_pem
-    private_key = file(".ssh/${var.key_name}.pem")
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = tls_private_key.ssh_key.private_key_pem
     host        = aws_instance.linux_server.public_ip
     port        = var.ssh_port
   }
